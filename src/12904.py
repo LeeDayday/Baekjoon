2eb488cad1825a08d4c24b46e859e4983b2f538d
LeeDayday <dayoon0905@gmail.com>
2023-03-21 00:15:15 +0900

    add 12904.py

---
+++ src/12904.py
@@ -0,0 +1,19 @@
+# A와 B
+# https://www.acmicpc.net/problem/12904
+# 수학, 구현, 그리디
+
+# =======================================
+S = list(map(str, input()))
+T = list(map(str, input()))
+
+while len(S) != len(T):
+    if T[-1] == 'A':
+        T.pop()
+    elif T[-1] == 'B':
+        T.pop()
+        T = T[::-1]
+
+if S == T:
+    print(1)
+else:
+    print(0)
