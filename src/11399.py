f220d2e7ddd89d439f5623cef4fdedcfa2460830
LeeDayday <dayoon0905@gmail.com>
2023-03-21 23:27:51 +0900

    add 11399.py

---
+++ src/11399.py
@@ -0,0 +1,17 @@
+# ATM
+# https://www.acmicpc.net/problem/11399
+# 
+
+# =======================================
+n = int(input())
+p = list(map(int, input().split()))
+cnt = 0
+waiting_cnt = [0] * len(p)
+
+p.sort()
+
+for i in range(len(p)):
+  for j in range(i+1, len(p)):
+    waiting_cnt[j] += p[i]
+
+print(sum(p) + sum(waiting_cnt))
