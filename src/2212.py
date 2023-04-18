a28167aa50667518eb7bc4ec7647d89c3583dbd9
LeeDayday <dayoon0905@gmail.com>
2023-04-18 12:59:00 +0900

    add 2212.py

--- src/2212.py
+++ src/2212.py
@@ -9,13 +9,17 @@ n = int(sys.stdin.readline())
 k = int(sys.stdin.readline())
 sensors = list(map(int, sys.stdin.readline().split()))
 
+if k >= n:
+    print(0)
+    sys.exit()
+    
 sensors.sort()
 diff = []
 cnt = 0
-for i in range(n-1):
-    diff.append(sensors[i+1] - sensors[i])
+for i in range(1, n):
+    diff.append(sensors[i] - sensors[i-1])
 
 diff.sort(reverse=True)
-for i in range(k):
+for i in range(k-1):
     diff.pop(0)
 print(sum(diff))
