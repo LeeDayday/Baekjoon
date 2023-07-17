51cf5cb57868131e788799aceeaa8c1be3570e13
LeeDayday <dayoon0905@gmail.com>
2023-07-17 14:43:39 +0900

    add sol2 1259.py

--- src/1259.py
+++ src/1259.py
@@ -7,14 +7,10 @@ import sys
 input = sys.stdin.readline
 
 def check_palindrome(n):
-    i = 0
-    j = len(n) - 1
-    while i < j:
-        if n[i] != n[j]:
-            return "no"
-        i += 1
-        j -= 1
-    return "yes"
+    if n == n[::-1]:
+        return "yes"
+    else:
+        return "no"
 
 if __name__ == '__main__':
     while True:
