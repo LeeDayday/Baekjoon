328b12642f29cfe49dbd41395ef257ceaed43465
LeeDayday <dayoon0905@gmail.com>
2023-07-26 15:22:04 +0900

    add sol2 11866.py

--- src/11866.py
+++ src/11866.py
@@ -11,23 +11,12 @@ n, k = map(int, input().split())
 arr = [i for i in range(1, n+1)]
 result = []
 idx = 0
-while True:
-    # k 번째 순서 구하기
-    step = 0
-    while True:
-        # 제거되지 않은 요소만 고려
-        if arr[idx] != 0:
-            step += 1
-        if step == k:
-            break
-        # k 번째 순서에 도달할 때까지 검사
-        idx = (idx + 1) % n
+while arr:
+    idx += k - 1
+    if idx >= len(arr):
+        idx %= len(arr)
 
-    result.append(arr[idx])
-    # 방문 표시
-    arr[idx] = 0
-    # 요세푸스 순열을 모두 구한 경우 break
-    if len(result) == n:
-        break
+    result.append(arr.pop(idx))
+
     
 print("<" + ", ".join(str(x) for x in result) + ">")
