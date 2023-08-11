35338105737997a59b766a8da0aba7cefdf18143
LeeDayday <dayoon0905@gmail.com>
2023-08-11 16:54:31 +0900

    add sol2 11726.py

--- src/11726.py
+++ src/11726.py
@@ -8,11 +8,13 @@ from sys import stdin
 n = int(stdin.readline())
 dp = [0] * (n + 1) # idx 1 부터 유의미한 값
 dp[1] = 1
-
 if n > 1:
     dp[2] = 2
-
-for i in range(3, n+1):
+def sol(i, dp):
+    if dp[i-1] == 0:
+        sol(i-1, dp)
     dp[i] = (dp[i-1] + dp[i-2]) % 10007
 
+if n > 2:
+    sol(n, dp)
 print(dp[n])
