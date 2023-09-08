86c80a2cdae9f62708b904f12a67b4ecf91ba063
LeeDayday <dayoon0905@gmail.com>
2023-09-08 21:23:06 +0900

    add 1967.py

--- src/1967.py
+++ src/1967.py
@@ -1,6 +1,6 @@
 # 트리의 지름
 # https://www.acmicpc.net/problem/1967
-# 
+# 그래프 이론, 그래프 탘색, 트리, 깊이 우선 탐색
 
 # =======================================
 import sys
@@ -17,10 +17,18 @@ def bfs(num):
     while queue:
         num = queue.popleft()
         for node, weight in graph[num]:
-            if visited[num] is False:
+            if visited[node] is False:
                 queue.append(node)
                 visited[node] = True
-                max_list[node] = weight
+                max_list[node] = max(max_list[node], max_list[num] + weight)
+
+    max_i, max_d = 0, 0
+    for idx, num in enumerate(max_list):
+        if num > max_d:
+            max_d = num
+            max_i = idx
+
+    return max_i, max_d
 n = int(input())
 graph = [[]for _ in range(n+1)]
 
@@ -29,3 +37,8 @@ for _ in range(n-1):
     graph[start].append((end, weight))
     graph[end].append((start, weight))
 
+# root 노드에서부터 가장 먼 노드 구하기
+num, weight = bfs(1)
+# 위에서 구한 노드로부터 가장 먼 노드 구하기 
+num, weight = bfs(num)
+print(weight)
