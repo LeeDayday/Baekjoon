d01201a68980977d54d830b565571648436735e0
LeeDayday <dayoon0905@gmail.com>
2024-01-31 23:00:28 +0900

    add sol2 11399.py

--- src/11399.py
+++ src/11399.py
@@ -3,15 +3,20 @@
 # 그리디, 정렬
 
 # =======================================
-n = int(input())
-p = list(map(int, input().split()))
-cnt = 0
-waiting_cnt = [0] * len(p)
+import sys
+input = sys.stdin.readline
 
-p.sort()
+def get_min(n, arr):
+    result = 0
+    arr.sort()
 
-for i in range(len(p)):
-  for j in range(i+1, len(p)):
-    waiting_cnt[j] += p[i]
-
-print(sum(p) + sum(waiting_cnt))
+    for num in arr:
+        result += num * n
+        n -= 1
+    
+    print(result)
+    
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    get_min(n, arr)
