fc6fc49566066e77fc548d53e29c9676660a142a
LeeDayday <dayoon0905@gmail.com>
2024-02-25 23:03:58 +0900

    add sol2 10026.py

--- src/10026.py
+++ src/10026.py
@@ -5,59 +5,58 @@
 # =======================================
 import sys
 from collections import deque
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
 
-graph = []
-
-for i in range(n):
-    graph.append(list(sys.stdin.readline().rstrip()))
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-def bfs(x, y, visited, flag):
+def bfs(graph, visited, i, j, color):
     queue = deque()
-    queue.append((x, y))
-    visited[x][y] = True
-    color = graph[x][y]
+    queue.append((i, j))
+    visited[i][j] = True
 
     while queue:
-        x, y = queue.popleft()
-        
+        curr_i, curr_j = queue.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            new_i = curr_i + dx[i]
+            new_j = curr_j + dy[i]
             
-            if nx < 0 or ny < 0 or nx >= n or ny >= n:
+            if new_i < 0 or new_i >= n:
                 continue
+            if new_j < 0 or new_j >= n:
+                continue
+            if not visited[new_i][new_j] and graph[new_i][new_j] in color:
+                queue.append((new_i, new_j))
+                visited[new_i][new_j] = True
 
-            ncolor = graph[nx][ny]
+if __name__ == '__main__':
+    n = int(input())
+    graph = []
+    visited = [[False for _ in range(n)] for _ in range(n)]
 
-            if flag == 1 and color != 'B' and ncolor != 'B':
-                ncolor = color
+    for _ in range(n):
+        graph.append(input().rstrip())
 
-            if ncolor == color and visited[nx][ny] is False:
-                queue.append((nx, ny))
-                visited[nx][ny] = True
-
-# 적록 색약 x
-non_cnt = 0
-visited = [[False for _ in range(n)] for _ in range(n)]
-for i in range(n):
-    for j in range(n):
-        if visited[i][j] is False:
-            bfs(i, j, visited, 0)
-            non_cnt += 1
-
-# 적록 색약 o
-cnt = 0
-
-visited = [[False for _ in range(n)] for _ in range(n)]
-for i in range(n):
-    for j in range(n):
-        if visited[i][j] is False:
-            bfs(i, j, visited, 1)
-            cnt += 1
-
-print(non_cnt, cnt)
+    # 적록색약이 아닌 사람
+    cnt_non = 0
+    for i in range(n):
+        for j in range(n):
+            if not visited[i][j]:
+                bfs(graph, visited, i, j, graph[i][j])
+                cnt_non += 1
+    
+    # 방문 리스트 초기화
+    visited = [[False for _ in range(n)] for _ in range(n)]
+    
+    # 적록색약인 사람
+    cnt = 0
+    for i in range(n):
+        for j in range(n):
+            if not visited[i][j] and graph[i][j] != 'B':
+                bfs(graph, visited, i, j, ['R', 'G'])
+                cnt += 1
+            if not visited[i][j] and graph[i][j] == 'B':
+                bfs(graph, visited, i, j, 'B')
+                cnt += 1               
+    
+    print(cnt_non, cnt)
