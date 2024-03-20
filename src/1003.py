032c6abe19f9fb4c6a9810431ba86458777f4c58
LeeDayday <dayoon0905@gmail.com>
2024-03-20 23:00:12 +0900

    add sol2 1003.py

--- src/1003.py
+++ src/1003.py
@@ -6,29 +6,24 @@
 import sys
 input = sys.stdin.readline
 
+def get_cnt(dp, n):
+    if n <= 1:
+        return dp[n]
+    if dp[n-1] == 0:
+        dp[n-1] = get_cnt(dp, n-1)
+    return (dp[n-1][0]+dp[n-2][0], dp[n-1][1] + dp[n-2][1])
 
-def fibonacci(n):
-    length_zero = len(zero)
-    # 피보나치 수열로 더 표현할 수 있는 경우
-    if n >= length_zero:
-        for i in range(length_zero, n+1):
-            # f(n) = f(n-1) + f(n-2)
-            zero.append(zero[i-1] + zero[i-2])
-            one.append(one[i-1] + one[i-2])
-    print(f"{zero[n]} {one[n]}")
+def solution():
+    t = int(input())
+    dp = [0] * (41)
+    dp[0] = (1, 0)
+    dp[1] = (0, 1)
+    
+    for _ in range(t):
+        n = int(input())
+        print(*get_cnt(dp, n))
 
     
-        
-
 if __name__ == '__main__':
-    t = int(input())
-
-    # f(0) = f(0)
-    # f(1) = f(1)
-    # f(2) = f(1) + f(0) <- 2부터 규칙 적용
-    zero = [1, 0, 1]
-    one = [0, 1, 1]
-    for _ in range(t):
-        fibonacci(int(input()))
-        
+    solution()
 
