60e6ccee79befeee456ebea475e7dbcb71c1f350
LeeDayday <dayoon0905@gmail.com>
2024-07-02 20:41:34 +0900

    add 2748.py

---
+++ src/2748.py
@@ -0,0 +1,19 @@
+# 피보나치 수 2
+# https://www.acmicpc.net/problem/2748
+# 수학, 다이나믹 프로그래밍
+
+# =======================================
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [0] * (91)
+
+dp[1] = 1
+dp[2] = 1
+
+for i in range(3, n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n])
