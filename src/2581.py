cd914aee43f81dae04536e7b90a349fe6e18cf49
LeeDayday <dayoon0905@gmail.com>
2024-09-08 23:47:51 +0900

    add 2581.py sol2

--- src/2581.py
+++ src/2581.py
@@ -1,15 +1,16 @@
 # 소수
 # https://www.acmicpc.net/problem/2581
-# 
+# 수학, 정수론, 소수 판정
 
 # =======================================
 import sys
+import math
 input = sys.stdin.readline
 
 def is_prime_number(n):
     if n == 1:
         return False
-    for i in range(2, n):
+    for i in range(2, int(math.sqrt(n) + 1)):
         if n % i == 0:
             return False
     return True
