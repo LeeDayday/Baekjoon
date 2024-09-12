6e20e620bb7a67c47212011db46e388069c0909b
LeeDayday <dayoon0905@gmail.com>
2024-09-12 17:22:57 +0900

    add 14719.py sol2

--- src/14719.py
+++ src/14719.py
@@ -1,7 +1,7 @@
 # 빗물
 # https://www.acmicpc.net/problem/14719
 # 구현, 시뮬레이션
-
+# O(N)
 # =======================================
 import sys
 input = sys.stdin.readline
@@ -9,13 +9,25 @@ input = sys.stdin.readline
 h, w = map(int, input().split())
 heights = list(map(int, input().split()))
 
-total = 0 # 빗물의 총량
+# i번째 위치까지의 최대 높이
+left_max = [0] * w # 0 ~ i 중 최대 높이
+right_max = [0] * w # i ~ n-1 중 최대 높이
 
-for i in range(1, w - 1): # 양 끝에는 빗물이 고일 수 없으므로 제외
-    max_height_left = max(heights[:i])
-    max_height_right = max(heights[i+1:])
-    # heights[i] 에서 빗물이 고일 수 있는지 확인
-    compare_height = min(max_height_left, max_height_right)
-    if heights[i] < compare_height:
-        total += compare_height - heights[i]
+
+left_max[0] = heights[0] # 0 ~ 0 중 최대 높이는 자기 자신
+for i in range(1, w):
+    left_max[i] = max(left_max[i-1], heights[i])
+
+right_max[-1] = heights[-1]
+for i in range(w - 2, -1, -1):
+    right_max[i] = max(right_max[i + 1], heights[i])
+
+
+# 빗물 양 계산하기
+total = 0
+for i in range(1, w-1): # 양 끝엔 빗물이 고일 수 없음
+    compare = min(left_max[i], right_max[i])
+    if compare > heights[i]:
+        total += compare - heights[i]
+
 print(total)
