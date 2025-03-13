dc5c1fb64ccefb76152cc3a049834102b1c5d6d0
LeeDayday <dayoon0905@gmail.com>
2025-03-13 23:27:01 +0900

    review 부분합 solution

--- src/tmp.py
+++ src/tmp.py
@@ -1,39 +1,30 @@
-# 복습 - 멀티탭 스케줄링
-# https://www.acmicpc.net/problem/1700
+# 복습 - 부분합
+# https://www.acmicpc.net/problem/1806
 
-from collections import defaultdict, deque
 import sys
 input = sys.stdin.readline
 
-n, k = map(int, input().split()) # n: 멀티탭 구멍 개수, k: 전기용품 사용 횟수
-data = list(map(int, input().split())) # 전기 용품 사용 순서
+n, s = map(int, input().split()) # n: 수열의 길이, s: 부분합 기준
+data = list(map(int, input().split())) # 수열
 
-def solution(n, k, data):
-    answer = 0
-    using = set() # 사용 중인 멀티탭
-
-    for i, num in enumerate(data):
-        using.add(num)
-        # 플러그를 뽑아야 하는 경우
-        if len(using) > n: 
-            # 사용중인 플러그 중 가장 나중에 사용되거나, 사용되지 않는 플러그 제거
-            last_idx = 0
-            last_num = 0 # 제거할 플러그
-            for using_num in using:
-                try:
-                    idx = data[i:].index(using_num) # using_num이 이후에 사용될 경우의 idx
-                except:
-                    last_num = using_num # using_num은 이후에 다시 사용되지 않음
-                    break
-                if last_idx < idx:
-                    last_idx = idx
-                    last_num = using_num
-            
-            using.remove(last_num)
-            answer += 1
-
+def solution(n, s, data):
+    answer = float('inf')
+    start, end = 0, 0
+    cnt = data[start] # 누적합 (=sum(data[start:end]))
+    while end < n:
+        if cnt < s:
+            end += 1
+            if end < n:
+                cnt += data[end]
+        else:
+            answer = min(answer, end - start + 1)
+            cnt -= data[start]
+            start += 1
+    
+    if answer == float('inf'):
+        return 0
     return answer
 
-print(solution(n, k, data))
+print(solution(n, s, data))
 
 
