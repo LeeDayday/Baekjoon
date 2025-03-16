233a4ccdd187809302b5f1254f2c78a31d88725a
LeeDayday <dayoon0905@gmail.com>
2025-03-16 23:06:18 +0900

    review 부분 문자열 solution

--- src/tmp.py
+++ src/tmp.py
@@ -1,40 +1,16 @@
-# 복습 - 최소 스패닝 트리
-# https://www.acmicpc.net/problem/1197
+# 복습 - 부분 문자열
+# https://www.acmicpc.net/problem/16916
 
-import sys
+import sys, re
 input = sys.stdin.readline
 
-v, e = map(int, input().split())
-data = []
-for _ in range(e):
-    data.append(list(map(int, input().split())))
-
-data.sort(key= lambda x: x[2])
-
-def find_parent(parent, a):
-    if parent[a] != a:
-        parent[a] = find_parent(parent, parent[a])
-    return parent[a]
-
-
-def union(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
-    if a > b:
-        parent[b] = a
-    else:
-        parent[a] = b
-
-
+s = input().rstrip()
+p = input().rstrip()
 def solution():
-    answer = 0
-    parent = [i for i in range(v + 1)]
-    for s, e, weight in data:
-        if find_parent(parent, s) != find_parent(parent, e):
-            union(parent, s, e)
-            answer += weight
-
-    return answer
+    if p in s:
+        return 1
+    else:
+        return 0
 
 print(solution())
 
