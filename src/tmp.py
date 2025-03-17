95c670289417acc3bb7a455430693946d6a47855
LeeDayday <dayoon0905@gmail.com>
2025-03-18 00:15:22 +0900

    review 수들의 합 solution

--- src/tmp.py
+++ src/tmp.py
@@ -1,40 +1,17 @@
-# 복습 - 줄 세우기
-# https://www.acmicpc.net/problem/2252
+# 복습 - 수들의 합
+# https://www.acmicpc.net/problem/1789
 
-from collections import defaultdict, deque
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-data = defaultdict(list)
-indegree = [0] * (n + 1)
+s = int(input())
 
-for _ in range(m):
-    a, b = map(int, input().split()) # a가 b 앞에 서야 한다
-    data[a].append(b) # 인접 리스트 추가
-    indegree[b] += 1 # 진입 차수 추가
-
-def solution():
-    answer = []
-    queue = deque()
-    visited = [False] * (n + 1)
-    for i in range(1, n + 1):
-        # 진입 차수가 0인 정점 queue에 추가
-        if indegree[i] == 0:
-            queue.append(i)
-        
-    while queue:
-        node = queue.popleft()
-        answer.append(node)
-        visited[node] = True
-        for new_node in data[node]:
-            if not visited[new_node]:
-                indegree[new_node] -= 1
-                if indegree[new_node] == 0:
-                    queue.append(new_node)
+def solution(s):
+    # 1 ... n 까지의 합: (n * (n + 1)) / 2
+    answer = 1
+    while answer * (answer + 1) // 2 + answer < s:
+        answer += 1
 
     return answer
     
-
-print(*solution())
-
+print(solution(s))
