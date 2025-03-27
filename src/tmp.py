db8e42bb4da293c9e15d44f30389471b9de4b1b1
LeeDayday <dayoon0905@gmail.com>
2025-03-27 23:10:14 +0900

    review 바이러스 solution

--- src/tmp.py
+++ src/tmp.py
@@ -1,5 +1,5 @@
-# 복습 - 음식물 피하기
-# https://www.acmicpc.net/problem/1743
+# 복습 - 바이러스
+# https://www.acmicpc.net/problem/2606
 
 import sys
 from collections import deque, defaultdict
@@ -8,39 +8,27 @@ input = sys.stdin.readline
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-n, m, k = map(int, input().split())
+n = int(input())
+m = int(input())
 
-data = [[0] * (m) for _ in range(n)]
-positions = set()
-for _ in range(k):
-    r, c = map(int, input().split())
-    positions.add((r - 1, c - 1))
-    data[r - 1][c - 1] = 1
+data = defaultdict(list)
+for _ in range(m):
+    a, b = map(int, input().split())
+    data[a].append(b)
+    data[b].append(a)
 
 def solution():
-    answer = 0
-    visited = [[False] * (m) for _ in range(n)]
-    def bfs(x, y):
-        queue = deque([(x, y)])
-        visited[x][y] = True
-        result = 1
-        while queue:
-            curr_x, curr_y = queue.popleft()
-            for i in range(4):
-                new_x = curr_x + dx[i]
-                new_y = curr_y + dy[i]
-                if 0 <= new_x < n and 0 <= new_y < m:
-                    if not visited[new_x][new_y] and data[new_x][new_y] == 1:
-                        queue.append((new_x, new_y))
-                        visited[new_x][new_y] = True
-                        result += 1
-                    
-        return result
-    
-    for x, y in positions:
-        if not visited[x][y]:
-            answer = max(answer, bfs(x, y))
-
-    return answer
+    cnt = 0
+    queue = deque([1])
+    visited = [False] * (n + 1)
+    visited[1] = True
+    while queue:
+        node = queue.popleft()
+        for new_node in data[node]:
+            if not visited[new_node]:
+                cnt += 1
+                visited[new_node] = True
+                queue.append(new_node)
+    return cnt
 
 print(solution())
