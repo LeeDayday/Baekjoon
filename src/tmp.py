867fc97ccd20a372708a1e90b09f6501134184be
LeeDayday <dayoon0905@gmail.com>
2025-03-27 23:19:22 +0900

    review 바이러스 solution

--- src/tmp.py
+++ src/tmp.py
@@ -2,11 +2,8 @@
 # https://www.acmicpc.net/problem/2606
 
 import sys
-from collections import deque, defaultdict
+from collections import defaultdict
 input = sys.stdin.readline
-
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
 
 n = int(input())
 m = int(input())
@@ -19,16 +16,18 @@ for _ in range(m):
 
 def solution():
     cnt = 0
-    queue = deque([1])
+    stack = [1]
     visited = [False] * (n + 1)
-    visited[1] = True
-    while queue:
-        node = queue.popleft()
-        for new_node in data[node]:
-            if not visited[new_node]:
-                cnt += 1
-                visited[new_node] = True
-                queue.append(new_node)
-    return cnt
+    
+    while stack:
+        print(stack)
+        node = stack.pop()
+        if not visited[node]:
+            visited[node] = True
+            cnt += 1
+            for new_node in data[node]:
+                if not visited[new_node]:
+                    stack.append(new_node)
+    return cnt - 1
 
 print(solution())
