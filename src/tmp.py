7499a44312e45ba7f636f1a1acb37740fe6fd878
LeeDayday <dayoon0905@gmail.com>
2025-03-30 21:12:34 +0900

    review 숨바꼭질 3 solution

--- src/tmp.py
+++ src/tmp.py
@@ -1,5 +1,5 @@
-# 복습 - 숨바꼭질 2
-# https://www.acmicpc.net/problem/12851
+# 복습 - 숨바꼭질 3
+# https://www.acmicpc.net/problem/13549
 
 import sys
 from collections import deque
@@ -11,22 +11,28 @@ cnt = [0] * 100001
 
 queue = deque([n])
 time[n] = 0
-cnt[n] = 1
 
 while queue:
     curr_pos = queue.popleft()
     if curr_pos == k:
         break
-    for next_pos in (curr_pos - 1, curr_pos + 1, curr_pos * 2):
+    # 1초가 소요되는 이동
+    for next_pos in (curr_pos - 1, curr_pos + 1):
         if 0 <= next_pos < 100001:
             # 더 빠른 시간으로 방문하는 경우 (업데이트 필요)
             if time[next_pos] > time[curr_pos] + 1:
                 time[next_pos] = time[curr_pos] + 1
                 queue.append(next_pos)
-                cnt[next_pos] = cnt[curr_pos]
-            # 동일한 시간으로 방문하는 경우 (cnt ++)
-            elif time[next_pos] == time[curr_pos] + 1:
-                cnt[next_pos] += cnt[curr_pos]
+    # 0초가 소요되는 이동
+    next_pos = curr_pos * 2
+    if 0 <= next_pos < 100001:
+        # 더 빠른 시간으로 방문하는 경우 (업데이트 필요)
+        if time[next_pos] > time[curr_pos]:
+            time[next_pos] = time[curr_pos]
+            queue.append(next_pos)
+            cnt[next_pos] = cnt[curr_pos]
+        # 동일한 시간으로 방문하는 경우 (cnt ++)
+        elif time[next_pos] == time[curr_pos]:
+            cnt[next_pos] += cnt[curr_pos]      
 
 print(time[k])
-print(cnt[k])
