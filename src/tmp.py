52b78ac4e1bafa21ce84b3fe6046a91798988abc
LeeDayday <dayoon0905@gmail.com>
2025-04-09 23:38:00 +0900

    review 평범한 배낭 solution

--- src/tmp.py
+++ src/tmp.py
@@ -1,34 +1,21 @@
-# 복습 - BOJ 거리
-# https://www.acmicpc.net/problem/12026
+# 복습 - 평범한 배낭
+# https://www.acmicpc.net/problem/12865
 
-# O(n^2)
+# O(nk)
 
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-data = input().rstrip()
+n, k  = map(int, input().split())
+data = []
+for _ in range(n):
+    data.append(list(map(int, input().split())))
 
-dp = [float('inf')] * n
-dp[0] = 0
+dp = [0] * (k + 1)
 
-dx = ['B', 'O', 'J']
-def get_next_ch(curr_ch):
-    for i in range(3):
-        if dx[i] == curr_ch:
-            return dx[(i + 1) % 3]
+for w, v in data:
+    for j in range(k, w - 1, -1):
+        dp[j] = max(dp[j], dp[j - w] + v)
 
-def solution():
-    if data[0] != 'B':
-        return -1
-    for i in range(n):
-        new_ch = get_next_ch(data[i])
-        for j in range(i + 1, n):
-            if data[j] == new_ch:
-                dp[j] = min(dp[j], dp[i] + (j - i) * (j - i)) # (j - i)칸 만큼 jump
 
-    if dp[-1] == float('inf'):
-        return -1
-    return dp[-1]
-
-print(solution())
+print(dp[-1])
