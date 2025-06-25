ff352da47b3adde364d4ad9675ce0e933abd2d6d
LeeDayday <dayoon0905@gmail.com>
2025-06-25 17:59:06 +0900

    review 숨바꼭질 solution

--- src/tmp.py
+++ src/tmp.py
@@ -1,29 +1,26 @@
-# 복습 - 단어 공부
-# https://www.acmicpc.net/problem/1157
+# 복습 - 숨바꼭질
+# https://www.acmicpc.net/problem/1697
 
-# O(N)
+# O(V + E) => O(V + 3V) => O(V) (V 는 최대 100001)
 
 import sys
-from collections import Counter
+from collections import deque
 input = sys.stdin.readline
 
-data = input().rstrip()
+n, k = map(int, input().split())
+queue = deque()
+visited = [False] * (100001)
 
-counter = Counter(data.upper())
+# 동생 위치 > 수빈 위치 도달 bfs
+queue.append((k, 0)) # 현재 위치, 시간
+visited[k] = True
 
-max_count = -1
-max_char = ''
-duplicate = False
-
-for char, cnt in counter.items():
-    if cnt > max_count:
-        max_count = cnt
-        max_char = char
-        duplicate = False
-    elif cnt == max_count:
-        duplicate = True
-
-if duplicate:
-    print("?")
-else:
-    print(max_char)
+while queue:
+    curr_pos, curr_time = queue.popleft()
+    for next_pos in [curr_pos - 1, curr_pos + 1, curr_pos // 2 if curr_pos % 2 == 0 else -1]:
+        if next_pos == n:
+            print(curr_pos)
+            exit(0)
+        if 0 <= next_pos <= 100000 and not visited[next_pos]:
+            queue.append((next_pos, curr_time + 1))
+            visited[next_pos] = True
