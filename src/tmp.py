f6e92b4344c413a18019f4e1b52148791ac47ebf
LeeDayday <dayoon0905@gmail.com>
2025-07-14 23:29:48 +0900

    review 등수 구하기 solution

--- src/tmp.py
+++ src/tmp.py
@@ -1,32 +1,26 @@
-# 복습 - 부분합
-# https://www.acmicpc.net/problem/1806
+# 복습 - 등수 구하기
+# https://www.acmicpc.net/problem/1205
 
-# O(N)
+# O(NlogN)
 
 
 import sys
 input = sys.stdin.readline
 
-n, s = map(int, input().split())
+n, target, p = map(int, input().split())
+
+if n == 0:
+    print(1)
+    exit(0)
 data = list(map(int, input().split()))
 
-start, end = 0, 0
-answer = float('inf')
-sum_ = 0
-while end < n:
-    if sum_ < s:
-        sum_ += data[end]
-        end += 1
-    else:
-        answer = min(answer, end - start)
-        sum_ -= data[start]
-        start += 1
+data.append(target)
+data.sort(reverse=True)
 
-while sum_ >= s:
-    answer = min(answer, end - start)
-    sum_ -= data[start]
-    start += 1    
-if answer == float('inf'):
-    print(0)
+answer = data.index(target) + 1
+
+if (answer > p) or (n == p and data[-1] == target): # 랭킹 리스트가 꽉 차있을 때, 새 점수가 이전 점수보다 좋을 때만 점수가 바뀐다
+    print(-1)
 else:
     print(answer)
+    
