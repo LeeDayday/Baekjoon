b7b7fd531d7c71b74c61904bac0125fcc9554831
LeeDayday <dayoon0905@gmail.com>
2025-07-15 16:15:48 +0900

    review 등수 구하기 solution

--- src/tmp.py
+++ src/tmp.py
@@ -1,8 +1,7 @@
 # 복습 - 등수 구하기
 # https://www.acmicpc.net/problem/1205
 
-# O(NlogN)
-
+# O(N)
 
 import sys
 input = sys.stdin.readline
@@ -14,13 +13,23 @@ if n == 0:
     exit(0)
 data = list(map(int, input().split()))
 
-data.append(target)
-data.sort(reverse=True)
-
-answer = data.index(target) + 1
-
-if (answer > p) or (n == p and data[-1] == target): # 랭킹 리스트가 꽉 차있을 때, 새 점수가 이전 점수보다 좋을 때만 점수가 바뀐다
+# 랭킹 리스트가 꽉 찼을 때 (n == p), 새 점수가 이전 점수보다 더 좋을 때만 점수 바뀜
+if n == p and target == data[-1]:
     print(-1)
+    exit(0)
+
+# target 에 대한 등수 구하기
+rank = 1
+for score in data:
+    if target < score:
+        rank += 1
+    else:
+        break
+
+# 해당 등수가 랭킹 리스트(p) 에 들 수 있다면 출력
+if rank <= p:
+    print(rank)
 else:
-    print(answer)
-    
+    print(-1)
+        
+
