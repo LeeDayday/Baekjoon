1b6ba616e526e980a22175a45cc07f3b1289e29c
LeeDayday <dayoon0905@gmail.com>
2025-07-22 23:47:16 +0900

    review 공유기 설치 solution

--- src/tmp.py
+++ src/tmp.py
@@ -1,35 +1,39 @@
-# 복습 - 등수 구하기
-# https://www.acmicpc.net/problem/1205
+# 복습 - 공유기 설치
+# https://www.acmicpc.net/problem/2110
 
-# O(N)
+# (정렬: O(NlogN)) + (이분탐색 O(NlogD) ,D: max_dist)
 
 import sys
 input = sys.stdin.readline
 
-n, target, p = map(int, input().split())
+n, c = map(int, input().split())
+data = []
+for _ in range(n):
+    data.append(int(input()))
+data.sort() # 이분탐색 진행을 위한 정렬 수행
 
-if n == 0:
-    print(1)
-    exit(0)
-data = list(map(int, input().split()))
+min_dist = 1
+max_dist = data[-1] - data[0]
 
-# 랭킹 리스트가 꽉 찼을 때 (n == p), 새 점수가 이전 점수보다 더 좋을 때만 점수 바뀜
-if n == p and target == data[-1]:
-    print(-1)
-    exit(0)
+# 가장 인접한 두 공유기 사이의 최대 거리
+# 어떠한 조건을 만족하도록 하는 어떤 값을 최대화
+    # 직접 계산하는 방법 대신, 특정 값이 조건을 만족하는 지 판단 ... 특정값 늘려가며 판단
+answer = 0
+while min_dist <= max_dist:
+    mid_dist = (min_dist + max_dist) // 2
 
-# target 에 대한 등수 구하기
-rank = 1
-for score in data:
-    if target < score:
-        rank += 1
+    cnt = 1 # 설치한 공유기 수 (초기: data[0] 위치에 공유기 설치)
+    last_pos = data[0] # 마지막으로 설치한 공유기 위치
+    for i in range(1, n): # mid_dist 거리 만큼 공유기 설치
+        if data[i] >= last_pos + mid_dist:
+            last_pos = data[i]
+            cnt += 1
+    if cnt >= c: # 조건을 만족하는 경우, 거리 늘리기
+        min_dist = mid_dist + 1
+        answer = mid_dist
     else:
-        break
+        max_dist = mid_dist - 1 # 조건을 만족하지 않는 경우, 거리 줄이기
 
-# 해당 등수가 랭킹 리스트(p) 에 들 수 있다면 출력
-if rank <= p:
-    print(rank)
-else:
-    print(-1)
-        
+print(answer)
+
 
