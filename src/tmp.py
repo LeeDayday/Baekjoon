6b335d888bffa59d52640407469ba03eb1dc6199
LeeDayday <dayoon0905@gmail.com>
2025-07-30 20:42:32 +0900

    review 로봇 청소기 solution

--- src/tmp.py
+++ src/tmp.py
@@ -1,23 +1,48 @@
-# 복습 - 시험 감독
-# https://www.acmicpc.net/problem/13458
+# 복습 - 로봇 청소기
+# https://www.acmicpc.net/problem/14503
 
-# O(N)
+# O(NM)
 
 import sys
 input = sys.stdin.readline
 
-n = int(input()) # 시험장 개수
-data = list(map(int, input().split())) # 각 시험장에 있는 응시자 수
-b, c = map(int, input().split()) # 총감독관/부감독관이 한 시험장에서 감시할 수 있는 응시자 수
+# index 별로 북, 동, 남, 서를 가리킴
+dx = [-1, 0, 1, 0] 
+dy = [0, 1, 0, -1]
+
+n, m = map(int, input().split()) # 방의 크기
+r, c, d = map(int, input().split()) # (r, c): 로봇 청소기 초기 좌표, d: 로봇 청소기 초기 방향
+
+data = []
+for _ in range(n):
+    data.append(list(map(int, input().split())))
+
 
 answer = 0
-for num in data:
-    answer += 1
-    num -= b
-    if num > 0:
-        answer += num // c
-        if num % c != 0:
-            answer += 1
+while True:
+    if data[r][c] == 0:
+        data[r][c] = 2  # 청소
+        answer += 1
 
+    cleaned = False
+    for _ in range(4):
+        d = (d + 3) % 4  # 반시계 방향 회전
+        nx = r + dx[d]
+        ny = c + dy[d]
+        # 청소되지 않은 빈칸이 있는 경우 전진
+        if 0 <= nx < n and 0 <= ny < m and data[nx][ny] == 0:
+            r, c = nx, ny
+            cleaned = True
+            break
+
+    if not cleaned:
+        # 후진
+        back = (d + 2) % 4
+        nx = r + dx[back]
+        ny = c + dy[back]
+        if 0 <= nx < n and 0 <= ny < m and data[nx][ny] != 1:
+            r, c = nx, ny
+        else:
+            break
 
 print(answer)
