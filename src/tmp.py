73764b76624030106c2412e8da8fd6f3dd36845a
LeeDayday <dayoon0905@gmail.com>
2025-07-31 19:55:48 +0900

    review 연산자 끼워넣기 solution

--- src/tmp.py
+++ src/tmp.py
@@ -1,48 +1,38 @@
-# 복습 - 로봇 청소기
-# https://www.acmicpc.net/problem/14503
+# 복습 - 연산자 끼워넣기
+# https://www.acmicpc.net/problem/14888
 
-# O(NM)
+# O((N-1)!)
 
 import sys
 input = sys.stdin.readline
 
-# index 별로 북, 동, 남, 서를 가리킴
-dx = [-1, 0, 1, 0] 
-dy = [0, 1, 0, -1]
+n = int(input())
+data = list(map(int, input().split()))
+operators = list(map(int, input().split())) # 연산자 [+,  -,  x,  //] 의 개수
 
-n, m = map(int, input().split()) # 방의 크기
-r, c, d = map(int, input().split()) # (r, c): 로봇 청소기 초기 좌표, d: 로봇 청소기 초기 방향
+answer = [-float('inf'), float('inf')] # max, min
+def dfs(idx:int, total:int, plus:int, minus:int, mul:int, div:int) -> None:
+    global answer
+    if idx == n: # 연산 수행 종료
+        answer[0] = max(total, answer[0])
+        answer[1] = min(total, answer[1])
+        return
+    if plus:
+        dfs(idx + 1, total + data[idx], plus - 1, minus, mul, div)
+    if minus:
+        dfs(idx + 1, total - data[idx], plus, minus - 1, mul, div)
 
-data = []
-for _ in range(n):
-    data.append(list(map(int, input().split())))
+    if mul:
+        dfs(idx + 1, total * data[idx], plus, minus, mul - 1, div)
 
-
-answer = 0
-while True:
-    if data[r][c] == 0:
-        data[r][c] = 2  # 청소
-        answer += 1
-
-    cleaned = False
-    for _ in range(4):
-        d = (d + 3) % 4  # 반시계 방향 회전
-        nx = r + dx[d]
-        ny = c + dy[d]
-        # 청소되지 않은 빈칸이 있는 경우 전진
-        if 0 <= nx < n and 0 <= ny < m and data[nx][ny] == 0:
-            r, c = nx, ny
-            cleaned = True
-            break
-
-    if not cleaned:
-        # 후진
-        back = (d + 2) % 4
-        nx = r + dx[back]
-        ny = c + dy[back]
-        if 0 <= nx < n and 0 <= ny < m and data[nx][ny] != 1:
-            r, c = nx, ny
+    if div:
+        if total < 0:
+            dfs(idx + 1, -(-total // data[idx]), plus, minus, mul, div - 1)
         else:
-            break
+            dfs(idx + 1, total // data[idx], plus, minus, mul, div - 1)
 
-print(answer)
+
+dfs(1, data[0], *operators)
+
+for ans in answer:
+    print(ans)
