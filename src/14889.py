ad6bd7df725e4188fdbbaab05772a3e46a88844a
LeeDayday <dayoon0905@gmail.com>
2025-08-03 23:32:40 +0900

    add 스타트와 링크 solution 2

--- src/14889.py
+++ src/14889.py
@@ -14,21 +14,35 @@ data = []
 for _ in range(n):
     data.append(list(map(int, input().split())))
 
-min_gap = float('inf')
-for start in combinations(numbers, n // 2):
-    team = [0, 0]
-    link = []
-    for num in numbers:
-        if num not in start:
-            link.append(num)
+visited = [False] * n
+answer = float('inf')
 
-   # print(start, link)
-    for i in range(n // 2):
-        for j in range(i + 1, n // 2):
-            if i != j:
-                team[0] += data[start[i] - 1][start[j] - 1] + data[start[j] - 1][start[i] - 1]
-                team[1] += data[link[i] - 1][link[j] - 1] + data[link[j] - 1][link[i] - 1]
-    #print(f"{team}")
-    min_gap = min(min_gap, abs(team[0] - team[1]))
+def dfs(cnt, idx):
+    global answer
+    if answer == 0:
+        return
+    # 팀 구성이 이루어진 경우
+    if cnt == n // 2:
+        score = [0, 0] # 팀 1, 팀 2의 총점
+        for i in range(n):
+            for j in range(i + 1, n):
+                # i, j 모두 스타트 팀인 경우 
+                if visited[i] and visited[j]:
+                    score[0] += data[i][j] + data[j][i]
+                # i, j 모두 링크 팀인 경우
+                elif not visited[i] and not visited[j]:
+                    score[1] += data[i][j] + data[j][i]
+        answer = min(answer, abs(score[0] - score[1]))
+        return
+    
+    # 팀 구성이 이루어지지 않은 경우
+    for i in range(idx, n):
+        if not visited[i]:
+            visited[i] = True # i 번째 팀원을 스타트 팀에 포함
+            dfs(cnt + 1, i + 1)
+            visited[i] = False # 백 트래킹
 
-print(min_gap)
+visited[0] = True
+dfs(1, 1)
+print(answer)
+
